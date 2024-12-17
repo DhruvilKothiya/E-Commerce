73b4a5267ddf3a4f4dc22b9572cd06a5ca68f6c5
DhruvilKothiya <dhruvil.kothiya@fxis.ai>
2024-12-17 11:15:21 +0530

    fetch the token using JWt

---
+++ src/Components/screens/Regex.js
@@ -0,0 +1,2 @@
+// export const validEmail=new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9-]+.[a-zA-Z]$');
+export const validPassword=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{5,}$/ 
